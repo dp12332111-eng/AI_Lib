b627534c0b3509d8d786f87ccdb074ec2ad02edb
Kartikey_Verma <kartikey.is.verma@gmail.com>
2026-08-31 01:34:26 +0530

    Delete LibraryRecommendation.Mvc directory

--- LibraryRecommendation.Mvc/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace LibraryRecommendation.Mvc.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
